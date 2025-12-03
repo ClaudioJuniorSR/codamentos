68ca6d6c68c4b67c4ae4252b3fa5d9aaa3485ea3
ClaudioJuniorSR <juninhoclaudio425@gmail.com>
2025-12-02 21:09:15 -0300

    feat: Adicionar codigo olamundo e exercicio dois.

---
+++ dois.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    int idade = 19;
+    int quantidade = 1;
+    float altura = 1.87;
+    char letra = 'C';
+    char nome[10] = "Claudio"; 
+
+
+
+
+}
--- olamundo.c
+++ olamundo.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
 
+int main(){
+    printf("Olá, mundo!\n");
+
+    int soma = 0;
+    int A, B;
+    A = 22;
+    B = 28;
+    soma = A + B;
+    printf("A soma de A + B é %d", soma);
+    
+
+
+
+
+
+
+}
