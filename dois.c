50ade0abdc9c9f4919889618c367d8f9f807083f
ClaudioJuniorSR <juninhoclaudio425@gmail.com>
2025-12-05 21:02:49 -0300

    aaa

--- dois.c
+++ dois.c
@@ -4,10 +4,27 @@ int main(){
     int idade = 19;
     int quantidade = 1;
     float altura = 1.87;
-    char letra = 'C';
+    char opção = 'C';
     char nome[10] = "Claudio"; 
 
+    printf("O nome é: %s \n", nome);
+    printf("A idade é: %d anos\n", idade);
+    printf("A altura é: %.2f metros\n", altura);
+    printf("A opção é: %c\n", opção);
 
+    /*
+    printf("%formato1 %formato2 %formato3", variavel1, varialvel2, variavel3) 
+    
+    ("\n") Para pular linha.
+
+    %d: Número inteiro. 1,2,3,4,5,6... (int)
+    %i: Equivalente a %d.  (int)
+    %f: Número decimal, 0.1,0.2,0.3,0.4... (float)
+    %e: Número decimal em notação científica, 1.870000e+00. (float ou double)
+    %c: Apenas um caractere. (char)
+    %s: Um conjunto de letras. (char(string)
+
+   */
 
 
 }
---
+++ tres.c
@@ -0,0 +1,37 @@
+#include <stdio.h> 
+#include <string.h>
+
+int main(){
+    char nome[20];
+    char fundador[100];
+    int quantidade;
+    float tamanho;
+    float preco;
+    
+    printf("Fale uma marca de veículos: ");
+    scanf("%s", nome);
+    printf("A marca de veículo escolhida é: %s \n", nome);
+
+    printf("Quantos carros a %s tem? ", nome);
+    scanf("%d", &quantidade);
+    printf("A %s tem %d carros\n", nome, quantidade);
+
+    printf("Qual a altura media dos carros da %s: ", nome);
+    scanf("%f", &tamanho);
+    printf("O tamanho médio dos carros da %s é de %.2f metros\n", nome, tamanho);
+
+    printf("Qual o valor médio dos carros da %s? ", nome);
+    scanf("%f", &preco);
+    printf("O valor médio dos %d carros da %s, com %.2f metros é de R$ %.3f mil reais\n", quantidade, nome, tamanho, preco);
+    
+    
+    getchar(); // limpa o ENTER deixado pelo scanf anterior
+
+    printf("Qual o nome do fundador da %s? ", nome);
+    fgets(fundador, sizeof(fundador), stdin);
+    fundador[strcspn(fundador, "\n")] = '\0';
+    printf("%s é o gênio e fundador da %s.\n", fundador, nome);
+
+    // Sempre usar o & para que o scanf salve a informação e use-a posteriomente.
+
+}
