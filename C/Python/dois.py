7e8bdee9e27a1a1e1d403da6e5b0ca66506610cf
ClaudioJuniorSR <juninhoclaudio425@gmail.com>
2025-12-05 22:11:24 -0300

    05/12/2025

---
+++ C/Python/dois.py
@@ -0,0 +1,16 @@
+#Trabalhando com variáveis
+#Operando números matematicamente
+#Inserindo valores e definindo variáveis
+x=int(input('Insira um valor inteiro de 0 a 9: '))
+print('O valor inteiro escolhido foi:  ',x)
+y=int(input('Isira um valor inteiro negativo de -1 a -9: '))
+#Definindo variáveis das operaçães matemáticas
+A=x+y   
+B=x-y
+C=x*y
+D=x/y
+#Retornando os resultados requeridos para o usuário
+print('A soma de x e y é:',A)
+print('A subtração entre x e y é:',B)
+print('O produto entre x e y é:',C)
+print('A razão entre x e y é:',D)
